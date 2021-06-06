55138d525a7f66c13813d315af80295a634231af
kathiravan <65820510+kathiravan31@users.noreply.github.com>
2021-06-06 06:48:37 -0700

    first commit

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,40 @@
+import React,{useEffect} from 'react'
+import Product from '../../components/Product'
+import './HomeScreen.css'
+import {useSelector,useDispatch} from 'react-redux'
+
+// Actions
+
+import {getProducts as listProducts} from '../../redux/actions/productActions'
+
+function HomeScreen() {
+
+    const dispatch = useDispatch();
+
+    const getProducts = useSelector(state => state.getProducts);
+
+    const {products, loading, error} = getProducts;
+
+    useEffect(()=>{
+        dispatch(listProducts())
+    },[dispatch])
+    return (
+        <div className="homescreen">
+            <h2 className="homescreen__title">Latest Products</h2>
+            <div className="homescreen__products">
+                {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> : products.map((product) =>(
+                    <Product
+                    key={product._id}
+                    productId={product._id}
+                    name={product.name}
+                    price={product.price}
+                    description={product.description}
+                    imageUrl={product.imageUrl}
+                    />
+                    ))}
+            </div>
+        </div>
+    )
+}
+
+export default HomeScreen
